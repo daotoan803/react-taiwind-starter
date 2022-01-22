83e946db9a10b846cf9f731b6f79c735e339b8ed
= <=>
2022-01-22 11:38:39 +0700

    add eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl text-pink-500 text-center">Hello world</h1>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="bg-slate-300">
+      <h1 className="text-2xl text-yellow-600">This is home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
